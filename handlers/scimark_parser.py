1c11c2a9f3af725fbdeff49feefb42101efa25c8
hansy <hansy0419@thundersoft.com>
2017-12-28 15:09:50 +0800

    update code

--- handlers/scimark_parser.py
+++ handlers/scimark_parser.py
@@ -2,6 +2,8 @@
 
 import re
 import string
+import json
+from caliper.server.run import parser_log
 
 def scimark_parser(content, outfp):
     score = -1
@@ -19,11 +21,35 @@ def scimark_parser(content, outfp):
             score = score_latter
         return score
 
+def scimark(filePath, outfp):
+    cases = parser_log.parseData(filePath)
+    result = []
+    for case in cases:
+        caseDict = {}
+        caseDict[parser_log.BOTTOM] = parser_log.getBottom(case)
+        titleGroup = re.search('\[test:([\s\S]+)scimark2', case)
+        if titleGroup != None:
+            caseDict[parser_log.TOP] = titleGroup.group(0)
+            caseDict[parser_log.BOTTOM] = parser_log.getBottom(case)
+        tables = []
+        tableContent = {}
+        centerTopGroup = re.search("\*\*\n([\s\S]+)kenel\.?", case)
+        tableContent[parser_log.CENTER_TOP] = centerTopGroup.groups()[0]
+        tableGroup = re.search("kenel\.*([\s\S]+\))", case)
+        if tableGroup is not None:
+            tableGroupContent_temp = tableGroup.groups()[0].strip()
+            tableGroupContent = re.sub(':([\s]+)Mflops:', ' Mflops:', tableGroupContent_temp)
+            table = parser_log.parseTable(tableGroupContent, ":{1,}")
+            tableContent[parser_log.I_TABLE] = table
+        tables.append(tableContent)
+        caseDict[parser_log.TABLES] = tables
+        result.append(caseDict)
+    outfp.write(json.dumps(result))
+    return result
 
 if __name__ == "__main__":
-    infp = open("1.ttx", "r")
-    content = infp.read()
-    outfp = open("2.txt", "a+")
-    scimark_parser(content, outfp)
+    infile = "scimark_output.log"
+    outfile = "scimark_json.txt"
+    outfp = open(outfile, "a+")
+    scimark(infile, outfp)
     outfp.close()
-    infp.close()
